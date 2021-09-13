9ab66e9b1247a634b0606a1567e2f2a84c97e354
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-09-13 22:15:43 +0900

    add pagination

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
-import { API_URL } from '@/config/index'
+import { API_URL, PER_PAGE } from '@/config/index'
+import Pagination from '@/components/Pagination'
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
+  const lastPage = Math.ceil(total / PER_PAGE)
+
   return (
     <Layout>
       <h1>Events</h1>
@@ -12,16 +16,26 @@ export default function EventsPage({ events }) {
           {evt.name}
         </EventItem>
       ))}
+
+      <Pagination page={page} total={total} />
     </Layout>
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`)
-  const events = await res.json()
+export async function getServerSideProps({ query: { page = 1 } }) {
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE
+
+  // Fetch total/count
+  const totaltRes = await fetch(`${API_URL}/events/count`)
+  const total = await totaltRes.json()
+
+  // Fetch events
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  )
+  const events = await eventRes.json()
 
   return {
-    props: { events: events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   }
 }
